1013791e813a206c9e8b41f02716a03c486c277c
Daniel <dchoi3@my.apsu.edu>
2017-04-21 22:37:55 -0500

    Tweaks

--- app/src/main/java/com/danielchoi/ternionfinal/AboutActivity.java
+++ app/src/main/java/com/danielchoi/ternionfinal/AboutActivity.java
@@ -1,8 +1,10 @@
 package com.danielchoi.ternionfinal;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class AboutActivity extends AppCompatActivity {
 
@@ -10,6 +12,6 @@ public class AboutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
-
+        ((TextView) findViewById(R.id.developers)).setTypeface(Typeface.createFromAsset(getAssets(),  "fonts/britanic.TTF"));
     }
 }
